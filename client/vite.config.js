dddf15f8f35c122026f3c199bf8951ccb0c25f07
mohmmad mazid <mazidmansury5500@gmail.com>
2026-08-17 12:10:32 +0530

    backend url changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        // target: "http://localhost:5000",
+        target: "https://afyacare-chronic-disease-management.onrender.com",
         changeOrigin: true,
       },
     },
